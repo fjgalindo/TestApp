f971dc074ed6ce6efb165f1be47ca585166509ca
♠ JG ♠ <fjgalindo@iesfuentesanluis.org>
2018-02-06 09:40:19 +0100

    Initial commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,70 @@
-angular.module('starter.controllers', [])
+angular.module('app.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+    .controller('TestCtrl', ['$scope', 'Question', 'preguntas', '$ionicModal', function ($scope, Question, preguntas, $ionicModal) {
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+        $ionicModal.fromTemplateUrl('templates/resume.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+        for (var i = 0; i < preguntas.length; i++) {
+            Question = preguntas[i];
+        }
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+        $scope.validAnswers = 0;
+        $scope.userStatus = 'looser';
+        $scope.userCount = 0;
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+        var updateUserStatus = function () {
+            var avgValidAnswers = ($scope.validAnswers / $scope.preguntas.length) * 100;
+            if (avgValidAnswers == 0) {
+                $scope.userStatus = 'looser';
+            } else if (avgValidAnswers == 100) {
+                $scope.userStatus = 'guru';
+            } else {
+                $scope.userStatus = 'poor';
+            }
+        }
+
+        var checkCompleted = function () {
+            if ($scope.preguntas.length == $scope.userCount) {
+                $scope.modal.show();
+                //window.location.href = '#/home';
+            }
+        }
+
+        $scope.reset = function () {
+            $scope.modal.remove();
+            location.reload();
+        }
+
+        $scope.validate = function (pregunta, idAnswer) {
+            if (pregunta.userAnswer == null) {
+                pregunta.userAnswer = idAnswer;
+                $scope.userCount++;
+                if (pregunta.validAnswer == idAnswer) {
+                    $scope.validAnswers++;
+                    pregunta.status = "correct";
+                } else {
+                    pregunta.status = "wrong";
+                }
+
+                updateUserStatus();
+                checkCompleted();
+
+                console.log(pregunta.status);
+            }
+        }
+
+
+        $scope.preguntas = preguntas;
+}]);
